0d608df51e4acc49a5a70129af5474200d474382
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2022-03-31 11:42:10 -0700

    Add a little bit of custom error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+use rocket::response::status;
 use rocket::serde::json;
 
 use rocket_sync_db_pools::{database, diesel};
@@ -22,6 +23,7 @@ async fn create_user(
     let new_user = conn
         .run(move |c| db::create_user_struct(c, &input))
         .await
+        // TODO: Return a 400 when we get a constraint violation
         .unwrap();
 
     json::Json(new_user)
@@ -30,9 +32,15 @@ async fn create_user(
 #[database("rps")]
 struct RpsDatabaseConnection(diesel::PgConnection);
 
+#[catch(404)]
+fn not_found() -> status::NotFound<()> {
+    status::NotFound(())
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![get_user_by_id, create_user])
+        .register("/", catchers![not_found])
         .attach(RpsDatabaseConnection::fairing())
 }
